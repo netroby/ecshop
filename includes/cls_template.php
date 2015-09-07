5bb0f915f6fa8d43a74b5f6d299d85892c87ff9d
ZhiFeng Hu <hufeng1987@gmail.com>
2015-09-07 19:48:54 +0800

    Fix array_shift problem
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- includes/cls_template.php
+++ includes/cls_template.php
@@ -419,7 +419,8 @@ class cls_template
         }
         else
         {
-            $tag_sel = array_shift(explode(' ', $tag));
+            $tag_arr = explode(' ', $tag);
+            $tag_sel = array_shift($tag_arr);
             switch ($tag_sel)
             {
                 case 'if':
